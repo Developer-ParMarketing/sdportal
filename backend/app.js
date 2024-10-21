75d2af9cff7df13dccc6cd3dad2cc306ddfb5ed2
Developer-ParMarketing <developer@parmarketing.agency>
2024-10-21 15:41:19 +0530

    enroll

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ app.use(
   cors({
     origin: [
       "https://client.singledebt.in",
+      "https://enroll.singledebt.in",
       "http://localhost:3000",
       "https://msg.mtalkz.com",
     ],
