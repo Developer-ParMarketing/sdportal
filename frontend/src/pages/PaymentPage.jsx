83985e51e23a54bd62e6d2ded2b8b942082b5575
Developer-ParMarketing <developer@parmarketing.agency>
2024-10-22 13:31:11 +0530

    Update PaymentPage.jsx

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -58,7 +58,7 @@ const PaymentPage = () => {
   const handlePayment = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8001/api/payment/orders", {
+      const response = await fetch("https://enroll.singledebt.in/api/payment/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
